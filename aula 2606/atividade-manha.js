d8c8de81fe2225d9db336dd84a2125f3822370b0
“AdrielMarx <adrielmarrx@gmail.com>
2024-06-26 11:43:47 -0300

    carrying out the 26-06 morning task

--- aula 2606/atividade-manha.js
+++ aula 2606/atividade-manha.js
@@ -1,9 +1,10 @@
 // Exercício 1
-let string = 'Jamelão';
-let stringSplit = string.split('');
-let splitReverse = stringSplit.reverse();
-let reverseJoin = splitReverse.join('');
-console.log(reverseJoin);
+let ola = "hello";
+let palavraInvertida =''
+for (let i = ola.length -1; i >=0; i--){
+    palavraInvertida += ola[i]
+}
+console.log(palavraInvertida)
 
 // Exercício 2
 let frase = 'As did I, we drink to die, we drink tonight';
